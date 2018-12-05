4d699d11725942a3618b06c1d63188145761524a
Victor Gago do Nascimento <victorgagonasc@gmail.com>
2018-12-05 00:07:45 -0200

    Added auth service

---
+++ src/providers/auth/auth.ts
@@ -0,0 +1,50 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import * as moment from "moment";
+import { config } from '../../app/app-config';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthProvider {
+
+  constructor(public http: HttpClient) { }
+
+  login(user): Observable<any> {
+    return this.http.post(config.USER_URL + '/users/authenticate', user).pipe(
+        map((res: any) => this.setSession(res.data))
+    );
+  }
+
+    private setSession(authResult) {
+        const expiresAt = moment().add(authResult.expiresIn, 'second');
+
+        localStorage.setItem('id_token', authResult.token);
+        localStorage.setItem('expires_at', JSON.stringify(expiresAt.valueOf()));
+    }
+
+    logout() {
+        localStorage.removeItem('id_token');
+        localStorage.removeItem('expires_at');
+    }
+
+    public isLoggedIn() {
+        return moment().isBefore(this.getExpiration());
+    }
+
+    isLoggedOut() {
+        return !this.isLoggedIn();
+    }
+
+    getToken() {
+        return localStorage.getItem('id_token');
+    }
+
+    getExpiration() {
+        const expiration = localStorage.getItem('expires_at');
+        const expiresAt = JSON.parse(expiration);
+        return moment(expiresAt);
+    }
+
+}
