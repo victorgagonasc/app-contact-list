02edcafdf9c02ddd9f89f724dd927caba35a710e
Victor Gago do Nascimento <victorgagonasc@gmail.com>
2018-12-06 00:33:40 -0200

    Configured auth token on interceptor

--- src/app/interceptors/token.interceptor.ts
+++ src/app/interceptors/token.interceptor.ts
@@ -10,6 +10,15 @@ export class TokenInterceptor implements HttpInterceptor {
     constructor(private auth: AuthProvider) { }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        let headers;
+
+        if (this.auth.isLoggedIn())
+            headers = { 'x-access-token': this.auth.getToken() }
+        else
+            headers = { 'Content-Type': 'application/json' }
+
+        request = request.clone({ setHeaders: headers });
+
         return next.handle(request);
     }
 }
