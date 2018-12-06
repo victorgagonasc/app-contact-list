90cb46df3ccd00c8254aceb81aa4e6fa58dfbe62
Victor Gago do Nascimento <victorgagonasc@gmail.com>
2018-12-06 00:32:31 -0200

    Added endpoint for contacts

--- src/app/app-config.ts
+++ src/app/app-config.ts
@@ -1,5 +1,6 @@
 const API_URL = 'http://localhost:3000';
 
 export const config: any = {
-    USER_URL: API_URL + '/users' 
+    USER_URL: API_URL + '/users',
+    CONTACT_URL: API_URL + '/contacts'
 }
