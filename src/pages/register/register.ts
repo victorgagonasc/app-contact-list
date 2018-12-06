e6c9d759bd61d96211093301e1421d10a99c475a
Victor Gago do Nascimento <victor.nascimento@venturus.org.br>
2018-12-06 09:37:35 -0200

    Added toast and loading controller to register feature

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -2,7 +2,7 @@ import { LoginPage } from './../login/login';
 import { AuthProvider } from './../../providers/auth/auth';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController, ToastController, LoadingController } from 'ionic-angular';
 import { UserProvider } from '../../providers/user/user';
 
 @IonicPage()
@@ -12,6 +12,7 @@ import { UserProvider } from '../../providers/user/user';
 })
 export class RegisterPage {
   registerForm;
+  isRecording = false;
 
   constructor(
     public navCtrl: NavController,
@@ -19,7 +20,9 @@ export class RegisterPage {
     private fb: FormBuilder,
     private auth: AuthProvider,
     private userApi: UserProvider,
-    private viewCtrl: ViewController
+    private viewCtrl: ViewController,
+    private loadingCtrl: LoadingController,
+    private toastCtrl: ToastController
   ) {
     this.registerForm = this.fb.group({
       name: ['', Validators.required],
@@ -35,15 +38,43 @@ export class RegisterPage {
       this.navCtrl.setRoot(LoginPage);
   }
 
+  showToast(message, status) {
+    const toast = this.toastCtrl.create({
+      message: message,
+      position: 'bottom',
+      duration: 3000
+    });
+
+    if (status == 'success') {
+      toast.onDidDismiss(() => {
+        this.isRecording = false;
+        this.goToLogin();
+      });
+    }
+
+    toast.present();
+  }
+
   register() {
-    this.userApi.register(this.registerForm.value).subscribe(
-      data => {
-        console.log(data);
-      },
-      err => {
-        console.error(err);
-      }
-    );
+    this.isRecording = true;
+
+    let loading = this.loadingCtrl.create({
+      content: 'Realizando login'
+    });
+
+    loading.present().then(() => {
+      this.userApi.register(this.registerForm.value).subscribe(
+        data => {
+          this.showToast('Cadastro efetuado com sucesso. Você será direcionado para a tela de login', 'success');
+          loading.dismiss();
+        },
+        err => {
+          loading.dismiss()
+          this.showToast(err.error.message, err.error.status);
+          this.isRecording = false;
+        }
+      );
+    });
   }
 
   goToLogin() {
